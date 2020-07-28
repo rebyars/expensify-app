f277180bdbc017ba64179f79f6b36abcdd240c74
rebyars <rebyars@crimson.ua.edu>
2020-07-28 17:40:17 -0500

    Add private routes

--- src/app.js
+++ src/app.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 import { Provider } from 'react-redux';
 
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
-import getExpensesTotal from './selectors/expenses-total';
-
+import { login, logout } from './actions/auth';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-import './firebase/firebase';
+import {firebase} from './firebase/firebase';
 
 const store = configureStore();
 console.log("testing");
@@ -21,10 +18,30 @@ const jsx = (
         <AppRouter />
     </Provider>
 );
+let hasRendered = false;
+const renderApp = () => {
+   if(!hasRendered){
+    ReactDOM.render(jsx, document.getElementById('app'));
+    hasRendered = true;
+   }
+};
 
 ReactDOM.render(<p>Loading...</p>, document.getElementById('app') );
 
-store.dispatch(startSetExpenses()).then(() => {
-    ReactDOM.render(jsx, document.getElementById('app') );
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        store.dispatch(login(user.uid));
+        console.log('uid', user.uid);
+        store.dispatch(startSetExpenses()).then(() => {
+            renderApp();
+        });
+        if(history.location.pathname === '/') {
+            history.push('/dashboard');
+        }
+    }else{
+        store.dispatch(logout());
+        renderApp();
+        history.push('/');
+    }
 });
 
